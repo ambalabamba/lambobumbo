c118618d0754335aa1aa8a7842b1c16b5f76283d
ambalabamba <38856804+ambalabamba@users.noreply.github.com>
2018-05-01 13:52:46 +0200

    Update schallywag.py

--- schallywag.py
+++ schallywag.py
@@ -3,7 +3,7 @@ import requests
 import random
 
 def spam():
-    threading.Timer(0.1, spam).start()
+    threading.Timer(0.001, spam).start()
     List = ['http://discourse.xgenstudios.com/t/forum-staff-impersonation/1579', 'http://discourse.xgenstudios.com/t/a-new-stick-rpg-is-what-we-all-want/1588', 'http://discourse.xgenstudios.com/t/season-1-discussion/1547']
     url = "".join(random.choice(List))
     requests.get(url)       
