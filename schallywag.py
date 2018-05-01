0766368ab0d9e94ceaab19088549d8c95d607071
ambalabamba <38856804+ambalabamba@users.noreply.github.com>
2018-05-01 13:43:48 +0200

    Create schallywag.py

---
+++ schallywag.py
@@ -0,0 +1,10 @@
+import threading
+import urllib.request
+import random
+
+def spam():
+    threading.Timer(0.1, spam).start()
+    List = ['http://discourse.xgenstudios.com/t/forum-staff-impersonation/1579', 'http://discourse.xgenstudios.com/t/a-new-stick-rpg-is-what-we-all-want/1588', 'http://discourse.xgenstudios.com/t/season-1-discussion/1547']
+    url = "".join(random.choice(List))
+    urllib.request.urlopen(url)       
+spam()
